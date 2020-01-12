e10c3d3eb4d0ac4c442f8f4e2ad3de6a94247c90
Sean Kenny <sean.kenny@hotmail.com>
2020-01-12 20:35:01 +0000

    Fix source path to be file from module
    I missed a / in the source path so puppet was treating it like a remote
    file where puppet:// is the protocol.
    
    Also missed a comma.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,21 @@
 class runqlat_to_graphite {
   file { '/root/cpu_latency.bt':
     ensure => present,
-    path   => '/root/cpu_latency.bt'
-    source =>  'puppet://modules/runqlat_to_graphite/cpu_latency.bt',
+    path   => '/root/cpu_latency.bt',
+    source => 'puppet:///modules/runqlat_to_graphite/cpu_latency.bt',
   }
 
   file { '/root/cpu_latency.rb':
     ensure => present,
     path   => '/root/cpu_latency.rb',
     mode   => '744',
-    source =>  'puppet://modules/runqlat_to_graphite/cpu_latency.rb',
+    source => 'puppet:///modules/runqlat_to_graphite/cpu_latency.rb',
   }
 
   file { '/etc/systemd/system/cpu_latency.service':
     ensure => present,
     path   => '/etc/systemd/system/cpu_latency.service',
-    source =>  'puppet://modules/runqlat_to_graphite/cpu_latency.service',
+    source => 'puppet:///modules/runqlat_to_graphite/cpu_latency.service',
   }
 
   service { 'start cpu_latency service':
